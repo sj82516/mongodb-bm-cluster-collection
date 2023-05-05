2147ef9bb6cc165f8f1f7cecaea4749102ccfb5d
yuanchieh <yuanchieh-at@Yuanchieh-3.local>
2023-05-05 09:04:44 +0800

    refactor: wrap in class

--- bm.rb
+++ bm.rb
@@ -6,12 +6,13 @@
 require 'benchmark'
 include Benchmark
 
-require_relative './mongo'
+require_relative './bm_cluster_collection'
 require_relative './generator'
 
-n = 1e6.to_i
-prepare_clustered_collection
-prepare_normal_collection
+n = 1e7.to_i
+client = mongo_client
+bm = BmClusterCollection.new(client)
+bm.prepare_collections
 gen = GenData.new
 
 10.times.each do
@@ -21,49 +22,50 @@ gen = GenData.new
 
     data = ids.zip(emails)
     x.report('clustered batch insert:') do
-      bm_insert!(CLUSTER_USERS, data)
+      bm.bm_insert!(CLUSTER_USERS, data)
     end
 
     x.report('batch insert:') do
-      bm_insert!(USERS, data)
+      bm.bm_insert!(USERS, data)
     end
 
     pp "-----------"
     search_size = n / 10
     search_ids = ids.sample(search_size).shuffle
+
     x.report('clustered find by id:') do
-      bm_find(CLUSTER_USERS, search_ids, nil)
+      bm.bm_find(CLUSTER_USERS, search_ids, nil)
     end
     x.report('find by id:') do
-      bm_find(USERS, search_ids, nil)
+      bm.bm_find(USERS, search_ids, nil)
     end
 
     pp "-----------"
     search_emails = emails.sample(search_size).shuffle
     x.report('clustered find by email:') do
-      bm_find(CLUSTER_USERS, nil, search_emails)
+      bm.bm_find(CLUSTER_USERS, nil, search_emails)
     end
     x.report('find by email:') do
-      bm_find(USERS, nil, search_emails)
+      bm.bm_find(USERS, nil, search_emails)
     end
 
     pp "-----------"
     delete_size = n / 100
     delete_ids = ids.sample(delete_size).shuffle
     x.report('clustered delete by id:') do
-      bm_delete(CLUSTER_USERS, delete_ids, nil)
+      bm.bm_delete(CLUSTER_USERS, delete_ids, nil)
     end
     x.report('delete by id:') do
-      bm_delete(USERS, delete_ids, nil)
+      bm.bm_delete(USERS, delete_ids, nil)
     end
 
     pp "-----------"
     delete_emails = emails.sample(delete_size).shuffle
     x.report('clustered delete by email:') do
-      bm_delete(CLUSTER_USERS, nil, delete_emails)
+      bm.bm_delete(CLUSTER_USERS, nil, delete_emails)
     end
     x.report('delete by email:') do
-      bm_delete(USERS, nil, delete_emails)
+      bm.bm_delete(USERS, nil, delete_emails)
     end
 
     pp "==========="
---
+++ bm_cluster_collection.rb
@@ -0,0 +1,100 @@
+require 'thread'
+require 'mongo'
+require 'faker'
+
+# just for convenience
+
+CLUSTER_USERS = :cluster_users
+USERS = :users
+
+def mongo_client
+  Mongo::Client.new(['localhost:27017'], database: 'test')
+end
+
+class BmClusterCollection
+  def initialize(client)
+    @client = client
+  end
+
+  def prepare_collections
+    prepare_clustered_collection
+    prepare_normal_collection
+  end
+
+  def prepare_clustered_collection
+    @client[CLUSTER_USERS].drop
+    @client[CLUSTER_USERS].create({
+      :clustered_index => {
+        :key => { :_id => 1 },
+        :unique => true,
+      }
+    })
+    bm_secondary_index(CLUSTER_USERS)
+  end
+
+  def prepare_normal_collection
+    @client[USERS].drop
+    @client[USERS].create
+    bm_secondary_index(USERS)
+  end
+
+  def bm_insert!(collection, data)
+    exec_func = lambda do |slice_data|
+      slice_data.each_slice(2000).to_a.each do |d|
+        documents = d.map { |r| { _id: r[0], email: r[1] } }
+        @client[collection].insert_many(documents)
+      end
+    end
+
+    run_in_parallel(data, exec_func)
+  end
+
+  def bm_secondary_index(collection)
+    @client[collection].indexes.create_one(
+      { email: 1 },
+      name: 'email_index',
+    )
+  end
+
+  def bm_find(collection, ids, emails)
+    data = ids || emails
+    exec_func = lambda do |data|
+      data.each do |d|
+        next @client[collection].find(_id: d).first unless ids.nil?
+        @client[collection].find(email: d).first
+      end
+    end
+
+    run_in_parallel(data, exec_func)
+  end
+
+  def bm_delete(collection, ids, emails)
+    data = ids || emails
+    exec_func = lambda do |slice_data|
+      slice_data.each do |data|
+        next @client[collection].delete_one(_id: data) unless data.nil?
+        @client[collection].delete_one(email: data)
+      end
+    end
+
+    run_in_parallel(data, exec_func)
+  end
+
+  private
+  def run_in_parallel(data, exec_func)
+    return if data.empty?
+
+    con_num = 4
+
+    threads = []
+
+    slice_data = data.each_slice(data.size / con_num)
+    con_num.times do |i|
+      threads << Thread.new do
+        exec_func.call(slice_data.to_a[i])
+      end
+    end
+
+    threads.each(&:join)
+  end
+end
